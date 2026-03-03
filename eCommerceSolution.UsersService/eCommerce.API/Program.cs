718e9219c0f8423a372e848e8a7f4fe139d72508
VipCode786 <v07860@gmail.com>
2026-03-03 15:30:39 +0530

    Swagger and Setup for angular frontend

--- eCommerceSolution.UsersService/eCommerce.API/Program.cs
+++ eCommerceSolution.UsersService/eCommerce.API/Program.cs
@@ -18,12 +18,32 @@ builder.Services.AddAutoMapper(typeof(ApplicationUserMappingProfile).Assembly);
 
 //fluent
 builder.Services.AddFluentValidationAutoValidation();
+
+//Add API explorer services
+builder.Services.AddEndpointsApiExplorer();
+
+//Add swagger generation services to create swagger specification
+builder.Services.AddSwaggerGen();
+
+//Add cors services
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder => {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
 //Build the web application
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
 //Routing
 app.UseRouting();
+app.UseSwagger(); //Adds endpoint that can serve the swagger.json
+app.UseSwaggerUI(); //Adds swagger UI (interactive page to explore and test API endpoints)
+app.UseCors();
 
 //Auth
 app.UseAuthentication();
