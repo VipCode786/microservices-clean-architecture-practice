4a35e980208c3195b9afca6546db792a5ddef617
VipCode786 <v07860@gmail.com>
2026-03-16 09:24:38 +0530

    Product microservices dependencies package middleware commit

---
+++ ecommerceSolution.ProductsService/ProductMicroService.API/Program.cs
@@ -0,0 +1,24 @@
+
+using BusinessLogicLayer;
+using DataAccessLayer;
+using FluentValidation.AspNetCore;
+using ProductMicroService.API.Middlewaree;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDataAccessLayer();
+builder.Services.AddBusinessLogicLayer();
+builder.Services.AddControllers();
+
+builder.Services.AddFluentValidationAutoValidation();
+var app = builder.Build();
+
+app.UseExceptionHandlingMiddleware();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run();
