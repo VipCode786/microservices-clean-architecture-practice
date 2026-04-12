5f3743c7aa38d21ceca485fb2b3ef8fc79917ece
VipCode786 <v07860@gmail.com>
2026-04-12 22:34:06 +0530

    swagger

--- ecommerceSolution.ProductsService/ProductMicroService.API/Program.cs
+++ ecommerceSolution.ProductsService/ProductMicroService.API/Program.cs
@@ -4,6 +4,7 @@ using DataAccessLayer;
 using FluentValidation.AspNetCore;
 using ProductMicroService.API.APIEndpoints;
 using ProductMicroService.API.Middlewaree;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +14,32 @@ builder.Services.AddBusinessLogicLayer();
 builder.Services.AddControllers();
 
 builder.Services.AddFluentValidationAutoValidation();
+//Add model binder to read values from JSON to enum
+builder.Services.ConfigureHttpJsonOptions(options => {
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+//Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Cors
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(builder => {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
 app.UseRouting();
+//Cors
+app.UseCors();
+
+//Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
